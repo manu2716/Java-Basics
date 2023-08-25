a5b7d7f882f4dec506155e0baad732a3b2b7dd83
Manu Yadav <manuyadav2716@gmail.com>
2023-08-25 17:09:24 +0100

    StringBuffer ops

--- src/StringBufferOps.java
+++ src/StringBufferOps.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class StringBufferOps {
+
+
+public class StringBufferOps{
+
+    public static void main(String[] args) {
+
+        StringBuffer sb = new StringBuffer();  // stringbuffer is a mutable and thread-safe class
+        sb.append("hello");
+
+        sb.reverse();
+        System.out.println(sb);
+
+    }
 }
