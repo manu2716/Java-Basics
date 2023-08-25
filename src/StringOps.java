6244ecd984a58fa1be6a50aa290afea9b46cdd2a
Manu Yadav <manuyadav2716@gmail.com>
2023-08-25 17:12:43 +0100

    String operations

--- src/StringOps.java
+++ src/StringOps.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class StringOps {
+public class StringOps {
+
+    public static void main(String[] args) {
+
+        String s = "Welcome";
+
+        System.out.println(s);
+
+        System.out.println(s.charAt(2));
+
+        System.out.println(s.toUpperCase());
+
+        System.out.println(s.toLowerCase());
+
+        System.out.println(s.replace('e','f'));
+    }
 }
