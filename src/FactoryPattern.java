5904eac6e0c99e7003ba8d0e4e8933780e0c67f7
Manu Yadav <manu.yadav@ing.com>
2025-12-23 15:52:08 +0100

    Factory Pattern

---
+++ src/FactoryPattern.java
@@ -0,0 +1,54 @@
+interface Shape {
+    void draw();
+}
+
+class Circle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Circle");
+    }
+}
+
+class Square implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Square");
+    }
+}
+
+class Rectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Rectangle");
+    }
+}
+
+class ShapeFactory {
+
+    public Shape getShape(String shapeType) {
+        if (shapeType == null) {
+            return null;
+        }
+        if (shapeType.equalsIgnoreCase("Circle")) {
+            return new Circle();
+        } else if (shapeType.equalsIgnoreCase("Square")) {
+            return new Square();
+        } else if (shapeType.equalsIgnoreCase("Rectangle")) {
+            return new Rectangle();
+        }
+        return null;
+    }
+}
+
+public class FactoryPattern {
+    public static void main(String[] args) {
+
+        ShapeFactory factory = new ShapeFactory();
+
+        Shape shape1 = factory.getShape("Circle");
+        shape1.draw();
+
+        Shape shape2 = factory.getShape("Square");
+        shape2.draw();
+    }
+}
